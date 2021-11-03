8cd4d89f220fcedbaf268fe23d94f6baf3aaf369
jmjry <jakeryan1998@icloud.com>
2021-11-03 15:45:54 +0000

    Initial commit with game summary print statements

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print("You’re attempting to pull off a heist.")
+print("You’ll need to navigate the bank, facing multiple challenges.")
+print("If you can reach the vault without alerting the guards, you’ll win.")
+
+print("You have four ways to break in")
+print("1, Back Alley")
+print("2, Roof")
+print("3, Underground")
+print("4, Pose as an Employee")
+
+playerChooses = input("Choose 1, 2, 3 or 4")
